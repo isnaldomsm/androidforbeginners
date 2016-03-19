60cfa7d2ff9b1e18e0992971738e0da9c2fa5e2e
isnaldomsm <isnaldomsm@gmail.com>
2016-03-19 01:10:53 -0300

    Problemas em pegar o clique na tela de elemento,porem sendo solucionado

--- app/src/main/java/com/matheus/isnaldo/calc/MainActivity.java
+++ app/src/main/java/com/matheus/isnaldo/calc/MainActivity.java
@@ -1,18 +1,22 @@
 package com.matheus.isnaldo.calc;
 
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+public class MainActivity extends Activity implements OnClickListener  {
 
-public class MainActivity extends AppCompatActivity {
+//public abstract class Calculadora extends Activity implements OnClickListener {
+
     /* duas variávies do tipo double pra efetuar as operações */
     private double operador1;
     private double operador2;
@@ -23,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
     /* componente de texto para o visor da calculadora */
     private EditText txt_visor;
 
-    /* componentes button que para o teclado numérico da calculadora */
-    private Button bt_0;
     private Button bt_1;
     private Button bt_2;
     private Button bt_3;
@@ -61,13 +63,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
     }
+
     private void inicializar() {
 
         txt_visor = (EditText) findViewById(R.id.txt_visor);
 
-        bt_0 = (Button) findViewById(R.id.bt_0);
+        Button bt_0 = (Button) findViewById(R.id.bt_0);
         bt_1 = (Button) findViewById(R.id.bt_1);
         bt_2 = (Button) findViewById(R.id.bt_2);
         bt_3 = (Button) findViewById(R.id.bt_3);
@@ -92,25 +94,191 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    //tecladonumerocp
+    private void tecladoNumerico(int leNumero) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        if (txt_visor.getText().toString().trim().equals("0.0")) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            limparVisor();
+            txt_visor.setText(txt_visor.getText().toString()
+                    + String.valueOf(leNumero));
+
+        } else {
+
+            txt_visor.setText(txt_visor.getText().toString()
+                    + String.valueOf(leNumero));
+
         }
 
-        return super.onOptionsItemSelected(item);
     }
-}
+
+    //Limpa tudo
+    private void limparVisor() {
+
+        txt_visor.setText("");
+
+    }
+
+    //ope necessaria
+    private void operacoes(String operadores) {
+
+        this.operadores = operadores;
+
+        if (operadores == "+") {
+
+            operador1 = Double.parseDouble(txt_visor.getText().toString().trim());
+            limparVisor();
+
+        } else if (operadores == "-") {
+
+            operador1 = Double.parseDouble(txt_visor.getText().toString().trim());
+            limparVisor();
+
+        } else if (operadores == "*") {
+
+            operador1 = Double.parseDouble(txt_visor.getText().toString().trim());
+            limparVisor();
+
+        } else if (operadores == "/") {
+
+            operador1 = Double.parseDouble(txt_visor.getText().toString().trim());
+            limparVisor();
+
+        }
+
+    }
+
+    //Verigualdade
+    private void igual() {
+
+        if (!txt_visor.getText().toString().trim().equals("")) {
+
+            if (operadores == "+") {
+
+                operador2 = operador1 + Double.parseDouble(txt_visor.getText().toString().trim());
+
+            } else if (operadores == "-") {
+
+                operador2 = operador1 - Double.parseDouble(txt_visor.getText().toString().trim());
+
+            } else if (operadores == "*") {
+
+                operador2 = operador1 * Double.parseDouble(txt_visor.getText().toString().trim());
+
+            } else if (operadores == "/") {
+
+                if (operador1 == 0) {
+
+                    operador2 = 0;
+
+                } else {
+
+                    operador2 = operador1 / Double.parseDouble(txt_visor.getText().toString().trim());
+
+                }
+
+            }
+
+        } else {
+
+            operador2 = 0;
+
+        }
+
+        txt_visor.setText(String.valueOf(operador2));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.bt_0:
+
+                tecladoNumerico(0);
+                break;
+
+            case R.id.bt_1:
+
+                tecladoNumerico(1);
+                break;
+
+            case R.id.bt_2:
+
+                tecladoNumerico(2);
+                break;
+
+            case R.id.bt_3:
+
+                tecladoNumerico(3);
+                break;
+
+            case R.id.bt_4:
+
+                tecladoNumerico(4);
+                break;
+
+            case R.id.bt_5:
+
+                tecladoNumerico(5);
+                break;
+
+            case R.id.bt_6:
+
+                tecladoNumerico(6);
+                break;
+
+            case R.id.bt_7:
+
+                tecladoNumerico(7);
+                break;
+
+            case R.id.bt_8:
+
+                tecladoNumerico(8);
+                break;
+
+            case R.id.bt_9:
+
+                tecladoNumerico(9);
+                break;
+
+            case R.id.bt_limpar:
+
+                limparVisor();
+                break;
+
+            case R.id.bt_igual:
+
+                igual();
+                break;
+
+
+
+
+            case R.id.bt_soma:
+
+                operacoes("+");
+                break;
+
+            case R.id.bt_subt:
+
+                operacoes("-");
+                break;
+
+            case R.id.bt_multi:
+
+                operacoes("*");
+                break;
+
+            case R.id.bt_divi:
+
+                operacoes("/");
+                break;
+
+        }
+
+    }
+
+     }
